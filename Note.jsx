817959ae7a9712f8ee18f55688304537a97b5767
Dabbeeru-Bhargavi <84859557+Dabbeeru-Bhargavi@users.noreply.github.com>
2021-06-06 22:58:38 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This bootcamp was taken up by saurya sinha sir,And This project was done by me.</p>
+        <p>I am Dabbeeru Bhargavi,an Engineering student from centurion university of technology and management</p>
+
+    </div>
+  );
+}
+
+export default Note;
